425c7dd1e60fa2fedb4b55c62568d36390c9b9a5
bturquet <baptiste.turquet@gmail.com>
2019-07-27 16:01:10 -0700

    styling admin cms

--- views_source/admin/config.js
+++ views_source/admin/config.js
@@ -1,1 +1,19 @@
+var PostPreview = createClass({
+    render: function() {
+      var entry = this.props.entry;
+      var image = entry.getIn(['data', 'featured_image']);
+      var bg = this.props.getAsset(image);
+      return h('div', {},
+        h('h1', {}, entry.getIn(['data', 'title'])),
+        h('p', {}, entry.getIn(['data', 'excerpt'])),
+        h('p', {}, "By Author Name"),
+        h('p', {}, entry.getIn(['data', 'category'])),
+        h('img', {src: bg.toString()}),
+        h('p', {}, entry.getIn(['data', 'image_caption'])),
+        h('div', {"className": "text"}, this.props.widgetFor('body'))
+      );
+    }
+  });
+
 CMS.registerPreviewStyle("document-preview.css");
+CMS.registerPreviewTemplate("posts", PostPreview);
